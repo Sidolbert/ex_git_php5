4063234938b71a9c9ed5d7b74e755d281277998f
Sidolbert <enrici.cyril@gmail.com>
2022-04-08 11:24:43 +0200

    ajout d'une fonction de bonjour à une promo

--- hello.php
+++ hello.php
@@ -5,3 +5,8 @@ echo "hello git !";
 echo "coucou php5 et surtout moi-même";
 echo "et c'est parti !";
 
+//fonction qui affiche un message de bonjour à une promo spécifique
+function bonjourPromo(string $name){
+    echo "bonjour à tous les membres de la promo " . $name;
+}
+
